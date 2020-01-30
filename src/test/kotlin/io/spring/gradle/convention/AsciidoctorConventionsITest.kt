d8c679f92ef345b14117bd74b4cec69e6b91ccc0
Rob Winch <rwinch@users.noreply.github.com>
2020-01-30 12:48:07 -0600

    Add multi backends test

--- src/test/kotlin/io/spring/gradle/convention/AsciidoctorConventionsITest.kt
+++ src/test/kotlin/io/spring/gradle/convention/AsciidoctorConventionsITest.kt
@@ -24,6 +24,18 @@ internal class AsciidoctorConventionsITest {
     }
 
     @Test
+    fun asciidocWhenMultipleBackendsThenSuccess() {
+        TestKit().use { testKit ->
+            val build = testKit
+                    .withProjectResource("convention/asciidoctor/backends")
+                    .withArguments(ASCIIDOCTOR_TASK_NAME)
+                    .forwardOutput()
+                    .build()
+            assertThat(build.task(ASCIIDOCTOR_TASK_NAME)?.outcome).isEqualTo(TaskOutcome.SUCCESS)
+        }
+    }
+
+    @Test
     fun asciidocWhenMissingAttributeThenFailure() {
         TestKit().use { testKit ->
             val build = testKit
